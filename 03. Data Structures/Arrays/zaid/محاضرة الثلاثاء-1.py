2691e8ca3822f6b4e67d5f185f26a17d1918ec8c
zaid123zaid123 <22310054@scme.edu.ps>
2024-11-05 09:01:06 +0200

    محاضرة الثلاثاء5/11

---
+++ 03. Data Structures/Arrays/zaid/محاضرة الثلاثاء-1.py
@@ -0,0 +1,25 @@
+#list1=[2,3,5,2,33,21]
+#i=0
+#while i < len(list1) :
+ #   print(list1[i])
+  #  i+=1
+
+#list1=[x for x in range(0,5)]
+#print(list1)
+#list2=[0.5 * x for x in list1]
+#print(list2)
+#list3=[x for x in list2 if x < 1.5]
+#print(list3)
+
+# list1=["green","red","blue"]
+# list2=["red","blue","green"]
+# print(list2==list1)
+# print(list2!=list1)
+# print(list2>=list1)
+# print(list2>list1)
+# print(list2<list1)
+# print(list2<=list1)
+# print(list2<=list1)
+
+items="welcome to the SCME".split()
+print(items)
