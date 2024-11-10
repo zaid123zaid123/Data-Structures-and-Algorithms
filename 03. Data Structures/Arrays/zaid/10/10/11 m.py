d95fffa4d915bd88b670456ecd63aa213e09a00d
zaid123zaid123 <22310054@scme.edu.ps>
2024-11-10 09:29:20 +0200

    محاضرة الثلاثاء10/11

---
+++ 03. Data Structures/Arrays/zaid/10/10/11 m.py
@@ -0,0 +1,56 @@
+
+# def reverse(list):
+#     result = []
+#     for element in list:
+#      result.insert(0, element)
+#     return result
+# print(reverse([1,2,3,4,5,6]))
+# def add(x, lst = []):
+#     if x not in lst:
+# #         lst.append(x)
+        
+#     return lst
+
+
+
+# def main():
+#     list1 = add(1)
+#     print(list1)
+
+#     list2 = add(2)
+#     print(list2)
+
+#     list3 = add(3, [11, 12, 13, 14])
+#     print(list3)
+
+#     list4 = add(4)
+#     print(list4)
+
+# main()
+
+# list3 = [2,3,5,7]
+# list3.reverse()
+# print('Reversed list:', list3)
+
+
+
+
+def linearSearch(lst, key):
+    for i in range(0, len(lst)):
+        if key == lst[i]:
+          return i
+         return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
